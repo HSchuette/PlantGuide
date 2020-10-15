06df41438bdbdc961a126c674c6ab5e17fae513a
HSchuette <hendrik.schuette@vaitrade.de>
2020-10-15 21:22:54 +0200

    Initial Commit

---
+++ PlantGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlantGuide
+//
+//  Created by (non work) on 15.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlantGuide/PlantGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantGuideApp.swift
+//  PlantGuide
+//
+//  Created by (non work) on 15.10.20.
+//
+
+import SwiftUI
+
+@main
+struct PlantGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
